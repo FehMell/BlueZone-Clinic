614b93a6526d322e19e588ea785635bb03483edc
Seu Nome Aqui <mell39654@gmail.com>
2025-11-05 23:03:26 -0300

    ultimos ajustes

--- frontend/src/components/serviceSection.jsx
+++ frontend/src/components/serviceSection.jsx
@@ -26,7 +26,7 @@ function ServicesSection() {
       ),
       icon: FaLeaf,
       showButton: true,
-      link: "/#/emagrecimento"
+      link: "/BlueZone-Clinic/#/emagrecimento"
     },
     {
       title: "Reposição Hormonal",
@@ -34,7 +34,7 @@ function ServicesSection() {
         "Indicada para homens com baixos níveis de testosterona. Melhora energia, libido e desempenho físico.",
       icon: FaHeartbeat,
       showButton: true,
-      link: "/#/reposicao-hormonal-masculina"
+      link: "/BlueZone-Clinic/#/reposicao-hormonal-masculina"
     },
     {
       title: "Hipertrofia Muscular e Alta Performance",
@@ -42,7 +42,7 @@ function ServicesSection() {
         "Plano personalizado para ganho de força e massa muscular, com orientações nutricionais e hormonais.",
       icon: FaDumbbell,
       showButton: true,
-      link: "/#/hipertrofia"
+      link: "/BlueZone-Clinic/#/hipertrofia"
     },
     {
       title: "Implantes Hormonais",
@@ -50,7 +50,7 @@ function ServicesSection() {
         "Implantes subcutâneos que equilibram hormônios e reduzem sintomas como cansaço e baixa libido.",
       icon: FaSyringe,
       showButton: true,
-      link: "/#/implantes-hormonais"
+      link: "/BlueZone-Clinic/#/implantes-hormonais"
     },
     {
       title: "Menopausa",
@@ -58,7 +58,7 @@ function ServicesSection() {
         "Tratamento que alivia sintomas da menopausa e melhora o bem-estar e a qualidade de vida das mulheres.",
       icon: FaHeart,
       showButton: true,
-      link: "/#/menopausa"
+      link: "/BlueZone-Clinic/#/menopausa"
     },
     {
       title: "Terapias Injetáveis",
@@ -66,7 +66,7 @@ function ServicesSection() {
         "Vitaminas, minerais e antioxidantes aplicados de forma personalizada para mais energia e vitalidade.",
       icon: FaSyringe,
       showButton: true,
-      link: "/#/terapias-injetaveis"
+      link: "/BlueZone-Clinic/#/terapias-injetaveis"
     },
     {
       title: "Bioimpedância",
@@ -95,7 +95,7 @@ function ServicesSection() {
         "Procedimentos que promovem firmeza, rejuvenescimento e valorizam sua autoestima.",
       icon: FaSpa,
       showButton: true,
-      link: "/#/estetica"
+      link: "/BlueZone-Clinic/#/estetica"
     }
   ];
 
@@ -193,7 +193,9 @@ function ServicesSection() {
           display: none;
         }
       `}</style>
+     
     </div>
+   
   );
 }
 
